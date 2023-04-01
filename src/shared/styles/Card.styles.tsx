a664d566db5537bc32b19771d17ca507b1702a6f
DankAvHi <thexcat86@gmail.com>
2023-04-01 15:36:33 +0300

    [crt] card mixins styles

---
+++ src/shared/styles/Card.styles.tsx
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+export const CardStyles = css`
+    border-radius: 12px;
+
+    padding: 12px;
+
+    background-color: #1a1a1a;
+    box-shadow: white 0 0 5px -2px;
+`;
