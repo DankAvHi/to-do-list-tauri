96ac0def07030bfc46555ebc473e40525469e9d0
DankAvHi <thexcat86@gmail.com>
2023-04-01 15:37:06 +0300

    [chng] default input styles

--- src/shared/UI/Input/Input.styles.tsx
+++ src/shared/UI/Input/Input.styles.tsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
 export const InputStyles = {
-    Input: styled.input``,
+    Input: styled.input`
+        border-radius: 2px;
+
+        padding: 8px 12px;
+
+        background-color: #111;
+    `,
 };
