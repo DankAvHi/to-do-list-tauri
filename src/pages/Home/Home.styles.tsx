81080dcd8cb0cd2b7d915afe95b1d21efea2f226
DankAvHi <thexcat86@gmail.com>
2023-04-01 15:36:04 +0300

    [crt] home page styles

---
+++ src/pages/Home/Home.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const HomeStyles = {
+    Home: styled.div`
+        margin: 0 auto;
+
+        max-width: 1024px;
+        width: 100%;
+
+        display: flex;
+        flex-direction: column;
+    `,
+};
